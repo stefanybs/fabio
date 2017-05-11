acbef8ecbe5b79810a9d54df7c5bf204c90ea44d
Leo Motta Rocha <leomottarocha@hotmail.com>
2017-05-11 17:10:02 -0300

    primeiro update

--- conteudo/cursos.php
+++ conteudo/cursos.php
@@ -1,22 +1,38 @@
-<script src="https://raw.githubusercontent.com/ganeshmax/jcarousellite/master/versions/jquery.jcarousellite-1.0.1.js"></script>
-<script src="js/jquery.js"></script>
-<script>
-  $(function(){
-    $(".carosel").jCarouselLite({
-      btnNext: ".next",
-      btnPrev: ".prev",
-      auto: 3000
-    })
-  })
-</script>
+<!--<div class="col-md-3">
+          <div class="box box-default collapsed-box">
+            <div class="box-header with-border">
+              <h3 class="box-title">
+
+              </h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                </button>
+              </div>
+              <!-- /.box-tools --
+            </div>
+            <!-- /.box-header --
+            <div class="box-body" style="display: none;">
+              The body of the box
+            </div>
+            <!-- /.box-body --
+          </div>
+          <!-- /.box --
+        </div>-->
+        <div class="col-sm-4 col-md-4 col-xs-12">
+        <div class="box box-default collapsed-box">
+            <div class="box-header with-border" style="height:120px;">
+			<img class="profile-user-img img-responsive img-circle" src="../../dist/img/curso1.png" width="120" height="120" style="float:left;">
+			<h3 style="float:right;">Vigilancia 1</h3>
+                <div class="box-tools pull-right">
+                	<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>Detalhes</button>
+            	</div>
+        	</div>
+            <div class="box-body text-justify" style="display: none;height:200px;">
+<b>Lorem Ipsum</b> é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. 
+            </div>
+        </div>
+        </div>
 
 
-<div class="carosel">
-<ul>
-<li><img src="dist/img/curso1.png" width="180" height="180" class="img-responsive img-circle pull-left" alt="Responsive image"></li>
-<li><img src="dist/img/avatar.png" width="180" height="180" class="img-responsive img-circle pull-left" alt="Responsive image"></li>
-</ul>
 
-<button class="prev">Anterior</button>
-<button class="next">Proximo</button>
-</div>
